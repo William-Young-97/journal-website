b595a838074e9ec4edde575ce4d8d856d2a1ce9f
tj <tomisinjoseph3@gmail.com>
2022-01-13 15:58:13 +0000

    Status code and index render tests pass

--- test/feature/index.test.js
+++ test/feature/index.test.js
@@ -4,7 +4,26 @@ const app = require("../../app");
 describe('Initial connections', () => {
   it("Makes a succesful get req to the hompage ", async () => {
     const res = await supertest(app)
-    .get("/"); // /api/index would be best practice - refactor this
-    expect(res.StatusCode).toEqual(200);
+      .get("/"); // /api/index would be best practice - refactor this
+    expect(res.status).toEqual(200);
   });
+
+  it("title says Hello World", async () => {
+    const res = await supertest(app)
+      .get("/")
+      .expect(200)
+      .expect((response) => {
+        response.text.includes('Hello World');
+      });
+  });
+
 });
+
+// it('should display the default page', async () => {
+//   await request(server)
+//    .get('/')
+//    .expect(200)
+//    .expect((response) => {
+//     assert.ok(response.text.includes('Hello World'));
+//    });
+//  });

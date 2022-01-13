afaf46baed45cfbc8f3e9e1ba789913aae382823
William Young <wadyoung@outlook.com>
2022-01-13 14:31:34 +0000

    Changed var to const; updated test path

--- test/feature/index.test.js
+++ test/feature/index.test.js
@@ -1,10 +1,11 @@
-const supertest = require("supertest")
+const supertest = require("supertest");
+const app = require("../../app")
 
 test("GET /", async () => {
   await supertest(app)
   .get("/") // /api/index would be best practice - refactor this
   .expect(200)
   .then((res) => {
-    expect(res.body[0].title).toContain('Hello World')
+    expect(res.body[0]).toContain('Hello World')
   })
 })
